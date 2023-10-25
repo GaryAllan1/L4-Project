e826d9cc9cdf646c4f28b940dbb94ae36302d6f3
Gary Allan <garyallan2010@hotmail.co.uk>
2023-10-25 21:10:27 +0100

    added quiz intro and signup pages with no content or funcitonality

--- src/haile/haileapp/urls.py
+++ src/haile/haileapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("study/", views.study, name="study"),
+    path("quiz_intro", views.quiz_intro, name="quiz_intro"),
+    path("signup", views.signup, name="signup"),
 ]
--- src/haile/haileapp/views.py
+++ src/haile/haileapp/views.py
@@ -4,3 +4,12 @@ from django.urls import reverse
 
 def index(request):
     return render(request, 'index.html')
+
+def study(request):
+    return render(request,'study.html')
+
+def quiz_intro(request):
+    return render(request,'quiz_intro.html')
+
+def signup(request):
+    return render(request,'signup.html')
