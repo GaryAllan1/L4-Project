c7bb8fa9e60137f7df03f2b8e0d6b73713883ac8
Gary Allan <garyallan2010@hotmail.co.uk>
2024-01-20 20:01:45 +0000

    added slash to static dir and static root

--- src/haile/haile/settings.py
+++ src/haile/haile/settings.py
@@ -17,8 +17,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIR = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
 # Quick-start development settings - unsuitable for production
