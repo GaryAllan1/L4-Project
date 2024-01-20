3ac2a0fc1584f36112a0feaef94bc51d3ba763a3
Gary Allan <garyallan2010@hotmail.co.uk>
2024-01-20 19:27:18 +0000

    change debug to false

--- src/haile/haile/settings.py
+++ src/haile/haile/settings.py
@@ -28,7 +28,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['46.101.85.80',
